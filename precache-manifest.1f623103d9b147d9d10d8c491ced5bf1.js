bf7e230e9dd7f7ccc5d4df20cdbfd4712c447d70
innerren <innerren@gmail.com>
2019-06-14 22:03:25 +0300

    Updates

---
+++ precache-manifest.1f623103d9b147d9d10d8c491ced5bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e62b45dee87e118b64022863c65ae5",
+    "url": "/fe_on_feb19-todo-app/index.html"
+  },
+  {
+    "revision": "a5efd27a8f2ab7f6dd1b",
+    "url": "/fe_on_feb19-todo-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fe_on_feb19-todo-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a5efd27a8f2ab7f6dd1b",
+    "url": "/fe_on_feb19-todo-app/static/js/main.65399998.chunk.js"
+  },
+  {
+    "revision": "c113e602fd9650e4e4c8",
+    "url": "/fe_on_feb19-todo-app/static/js/runtime~main.bc6d5655.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fe_on_feb19-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
