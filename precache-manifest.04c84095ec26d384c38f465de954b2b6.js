f7e0697f1e43a3c0ab2d84c0d5f09f27a96be1d8
innerren <innerren@gmail.com>
2019-07-30 13:21:48 +0300

    Updates

---
+++ precache-manifest.04c84095ec26d384c38f465de954b2b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1cc4d3775c2fb0a0e1f7831bd56139",
+    "url": "/fe_on_feb19-todo-app/index.html"
+  },
+  {
+    "revision": "a89872dab990d843256b",
+    "url": "/fe_on_feb19-todo-app/static/css/main.47049499.chunk.css"
+  },
+  {
+    "revision": "c18f8b2a7009a879e192",
+    "url": "/fe_on_feb19-todo-app/static/js/2.fe6f4c48.chunk.js"
+  },
+  {
+    "revision": "a89872dab990d843256b",
+    "url": "/fe_on_feb19-todo-app/static/js/main.1c011e2f.chunk.js"
+  },
+  {
+    "revision": "c113e602fd9650e4e4c8",
+    "url": "/fe_on_feb19-todo-app/static/js/runtime~main.bc6d5655.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f6125b52e885ed480c49158e06e50d.js"
+  "/fe_on_feb19-todo-app/precache-manifest.04c84095ec26d384c38f465de954b2b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fe_on_feb19-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
